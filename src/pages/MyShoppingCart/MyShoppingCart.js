ca010e650eedecfd20d2a229848418b3036346c7
drv0228 <110354201+drv0228@users.noreply.github.com>
2023-11-25 02:00:39 -0500

    refactoring modal

--- src/pages/MyShoppingCart/MyShoppingCart.js
+++ src/pages/MyShoppingCart/MyShoppingCart.js
@@ -4,13 +4,22 @@ import Modal from '../../components/Modal/Modal';
 
 const MyShoppingCart = ({cartItems, onRemoveFromCart, refresh, onClose, isModalOpen }) => {
   const totalProducts = cartItems.length;
+  function finalTotalAmount (array) {
+    let sum = 0;
+    for (let i=0; i < array.length; i++){
+      sum += array[i].price;
+    }
+    return sum;
+  }
+  const totalAmount = finalTotalAmount(cartItems);
+
   return (
     <>
     
       {isModalOpen && (
          <Modal onClose={onClose} refresh={refresh}>
       <p className="cart__products">Products in Cart</p>
-      <p className="products__amount">{totalProducts}</p>
+     
       <div className="cart-items-container">
       <div className="products-list">
         {totalProducts > 0 ? (
@@ -34,6 +43,8 @@ const MyShoppingCart = ({cartItems, onRemoveFromCart, refresh, onClose, isModalO
        
       </div>
       
+      <p className="products__amount">Subtotal  ({totalProducts}  Products)</p>
+      <h2 className="products__total">${totalAmount}</h2>
       </Modal>
       
       )}
